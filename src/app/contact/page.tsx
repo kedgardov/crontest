f4399162abd9805438c0404cbb709e22eab910a9
Ingeniando <ingeniando@192.168.1.8>
2025-10-06 00:25:21 -0700

    initiated contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Contact(){
+    return (
+        <div>
+            Contact Data
+            <Link href={'/'}>Back</Link>
+        </div>
+    )
+}
