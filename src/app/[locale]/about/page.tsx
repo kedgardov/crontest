a91408b0a42c5937dc3cb2b0390f366c54d6b8f6
Ingeniando <ingeniando@192.168.1.8>
2025-10-10 23:48:53 -0700

    added content to about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function About(){
     return (
-        <div className="w-full h-full flex flex-col items-center xl:flex-row border-t-4 gap-4 border-white/30">
+        <div className="w-full h-full flex flex-col items-center xl:flex-row border-t gap-4 border-white/30">
 
 
-            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-full flex flex-col trans">
+            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-[90dvh] flex flex-col trans">
                 <div className="h-3/5 flex items-center justify-center order-1">
-                    <div className="w-4/5 h-4/5 relative">
+                    <div className="w-4/5 h-4/5 relative shadow-2xl">
                         <Image
                             draggable={false}
                             className="object-cover object-center"
@@ -19,6 +19,7 @@ export default function About(){
                     </div>
                 </div>
                 <div className="h-2/5 flex items-center justify-center order-2">
+                    {/* <div className="w-4/5 h-4/5 [text-shadow:2_2_12_#000000]"> */}
                     <div className="w-4/5 h-4/5">
                         <h2 className="uppercase text-3xl text-highlight font-semibold tracking-wider">Education</h2>
                         <h3 className="uppercase text-white tracking-wider">College</h3>
@@ -35,9 +36,9 @@ export default function About(){
                 </div>
             </div>
 
-            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-full flex flex-col trans">
+            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-[90dvh] flex flex-col trans">
                 <div className="h-3/5 flex items-center justify-center order-1 xl:order-2 ">
-                    <div className="w-4/5 h-4/5 relative overflow-clip">
+                    <div className="w-4/5 h-4/5 relative overflow-clip shadow-2xl">
                         <Image
                             draggable={false}
                             className="object-cover object-center"
@@ -50,24 +51,21 @@ export default function About(){
                 </div>
                 <div className="h-2/5 flex items-center justify-center order-1 xl:order-1">
                     <div className="w-4/5 h-4/5 flex flex-col justify-end">
-                        <h2 className="uppercase text-3xl text-highlight font-semibold tracking-wider">Education</h2>
-                        <h3 className="uppercase text-white tracking-wider">College</h3>
+                        <h2 className="uppercase text-3xl text-highlight font-semibold tracking-wider">Hobbies</h2>
                         <ul className="list-disc list-inside text-white/65">
-                            <li>UACH - Chihuahua</li>
-                            <li>NMSU - New Mexico</li>
-                        </ul>
-                        <h3 className="uppercase text-white tracking-wider">Degrees</h3>
-                        <ul className="list-disc list-inside text-white/65">
-                            <li>Aerospace Engineering</li>
-                            <li>Mechanical Engineering</li>
+                            <li>Hiking</li>
+                            <li>Chess</li>
+                            <li>Basketball</li>
+                            <li>Mountain Biking</li>
+                            <li>Math Challenges</li>
                         </ul>
                     </div>
                 </div>
             </div>
 
-            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-full flex flex-col trans">
+            <div className="w-full max-w-172 xl:w-1/3 h-[70dvh] xl:h-[90dvh] flex flex-col trans">
                 <div className="h-3/5 flex items-center justify-center order-1">
-                    <div className="w-4/5 h-4/5 bg-white/40 relative">
+                    <div className="w-4/5 h-4/5 relative shadow-2xl">
                         <iframe
                             className="absolute top-0 left-0 w-full h-full object-cover"
                             src="https://www.youtube.com/embed/TtIPNrUdft0?si=e18RCu1RsfFKQktK&start=1"
@@ -80,17 +78,11 @@ export default function About(){
                 </div>
                 <div className="h-2/5 flex items-center justify-center order-2">
                     <div className="w-4/5 h-4/5">
-                        <h2 className="uppercase text-3xl text-highlight font-semibold tracking-wider">Education</h2>
-                        <h3 className="uppercase text-white tracking-wider">College</h3>
-                        <ul className="list-disc list-inside text-white/65">
-                            <li>UACH -  Chihuahua</li>
-                            <li>NMSU - New Mexico</li>
-                        </ul>
-                        <h3 className="uppercase text-white tracking-wider">Degrees</h3>
-                        <ul className="list-disc list-inside text-white/65">
-                            <li>Aerospace Engineering</li>
-                            <li>Mechanical Engineering</li>
-                        </ul>
+                        <h2 className="uppercase text-3xl text-highlight font-semibold tracking-wider">Skills</h2>
+                        <h3 className="uppercase text-white tracking-wider">Technologies</h3>
+                        <p className="text-white/65">Next.js, FastAPI, FlightPHP, SQL, OpenAPI, Docker, Git</p>
+                        <h3 className="uppercase text-white tracking-wider">Languages</h3>
+                        <p className="text-white/65">Spanish, English, Typescript Python, PHP, Go, Bash</p>
                     </div>
                 </div>
             </div>
